a2066de6b3551c8cade48bd453016e2ae8513ee5
19900410ms <57243729+19900410ms@users.noreply.github.com>
2020-02-29 10:26:13 +0900

    preview
    preview JS
    index画面画像が無い場合の処理
    投稿画面の調整

--- app/assets/javascripts/spots.js
+++ app/assets/javascripts/spots.js
@@ -1,3 +1,14 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$(function() {
+  var file_field = document.querySelector('input[type=file]')
+
+  $("#img-file").change(function() {
+    var file = $('input[type="file"]').prop('files')[0];
+    var fileReader = new FileReader();
+    fileReader.onload = function() {
+      var src = fileReader.result
+      var html = '<img src="${src}" width="120" height="80">'
+      $(".post-spot__form__bottom__preview").after(html);
+    }
+    fileReader.readAsDataURL(file);
+  });
+});
